af5f9b0595fb66b48d6e12eb92daef8fc20d9556
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-20 10:33:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# A Twitter Clone in Rails
+
+This **practice app** was built as part of [Tealeaf Academy's](http://www.gotealeaf.com) 'Rapid Prototyping with Ruby on Rails'.
+
+The purpose of building this Twitter clone is purely to get **more practice** with building a small Rails app end-to-end.
+
+### Rails version
+4.0.0
+
+### Learning Points
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterCloneRails::Application.config.secret_key_base = 'f335582f28c6278d697c1b3d6e07440b7eb13a9e801c6f64dcd55459f465e194e6ef25d817a6e3090d62152b9f22f175526e1c57604b1bc75c8916fc5d20f748'
